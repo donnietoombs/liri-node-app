69ba574d72f8f17654017ff34e4be50db2242a2f
Donnie Toombs <donnietoombs@gmail.com>
2018-09-12 02:33:48 -0400

    updates

--- liri.js
+++ liri.js
@@ -1,83 +1,108 @@
-// require("dotenv").config();
+require("dotenv").config();
 
-var keys = require("./keys")
+var keys = require("./keys");
 var request = require('request');
 
-
-// var spotify = require('spotify');
-
-
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 var fs = require('fs');
 
-
 var nodeArgv = process.argv;
-var command = process.argv[2];
-
-
+var action = process.argv[2];
 var searchTerm = "";
 
 // Deals with multiple length arguments
-for (var i=3; i<nodeArgv.length; i++){
-  if(i>3 && i<nodeArgv.length){
+for (var i = 3; i < nodeArgv.length; i++) {
+  if (i > 3 && i < nodeArgv.length) {
     searchTerm = searchTerm + "+" + nodeArgv[i];
-  } else{
+  } else {
     searchTerm = searchTerm + nodeArgv[i];
   }
 }
 
 
-//switch case
-switch(command){
-  
-    case "concert-this":
-    showbands()
-  break;
+//switch statement
+switch (action) {
 
-//   case "spotify-this-song":
-    // if(searchTerm){
-    //   spotifySong(searchTerm);
-    // } else{
-    //   spotifySong("I Saw The Sign");
-    // }
-//   break;
+  case "concert-this":
+    showBands()
+    break;
+
+  case "spotify-this-song":
+    if (searchTerm) {
+      spotifySearch(searchTerm);
+    }
+    else {
+      spotifySearch("I Saw The Sign");
+    }
+   break;
 
   case "movie-this":
-    if(searchTerm){
+    if (searchTerm) {
       movieSearch(searchTerm)
-    } else{
+    } else {
       omdbData("Mr. Nobody")
     }
-  break;
+    break;
 
   case "do-what-it-says":
-    doSomething();
-  break;
+    doThis();
+    break;
 
   default:
     console.log("{Please enter an action: concert-this, spotify-this-song, movie-this, do-what-it-says}");
-  break;
+    break;
 }
 
-function movieSearch(){
+function showBands() { }
+
+function spotifySearch(searchTerm) {
+
+  songTitle = searchTerm;
+
+  spotify.search({ type: 'track', query: songTitle }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    var usuableData= (JSON.stringify(data, null, 2));
     
-var movieName = searchTerm;
+    var artistsArr = data.tracks.items[0].album.artists;
+	var artistsNames = [];
+    for (var i = 0; i < artistsArr.length; i++) {
+			artistsNames.push(artistsArr[i].name);
+		}
+        var artists = artistsNames.join(", ");
 
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+		// Consoles out the artist(s), track name, preview url, and album name.
+		console.log("Artist(s): " + artists);
+		console.log("Song: " + data.tracks.items[0].name)
+		console.log("Spotify Preview URL: " + data.tracks.items[0].preview_url)
+		console.log("Album Name: " + data.tracks.items[0].album.name);
+	});
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+}
 
-request(queryUrl, function(error, response, body) {
-    console.log(response);
 
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
+function movieSearch() {
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    var body = JSON.parse(body);
+  var movieName = searchTerm;
+
+  // Then run a request to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+  // This line is just to help us debug against the actual URL.
+  console.log(queryUrl);
+
+  request(queryUrl, function (error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+
+      // Parse the body of the site and recover just the imdbRating
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      var body = JSON.parse(body);
       console.log("Title: " + body.Title);
       console.log("Release Year: " + body.Year);
       console.log("IMdB Rating: " + body.imdbRating);
@@ -86,7 +111,14 @@ request(queryUrl, function(error, response, body) {
       console.log("Language: " + body.Language);
       console.log("Plot: " + body.Plot);
       console.log("Actors: " + body.Actors);
-  }
-});
-
+    }
+  });
 }
+
+function doThis() {
+  fs.readFile('random.txt', "utf8", function(error, data){
+    var fileDataArr = data.split(',');
+
+    spotifySearch(fileDataArr[1]);
+  });
+};
